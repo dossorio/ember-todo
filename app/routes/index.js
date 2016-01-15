142086b7c592b7d95624f5cac3d1286525d4638c
Daniel Ossorio <daniel@ossorio.me>
2016-01-15 09:04:01 +0000

    moving hardcoded todos to ember model hook

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
+var todos = [
+  {desc: 'Buy bread'},
+  {desc: 'Be a better developer'},
+  {desc: 'Learn emberjs'},
+];
+
 export default Ember.Route.extend({
+  model(){
+    return todos;
+  },
 });
