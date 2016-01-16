01cc7ee0d364d74ee83485c8caf59c1715f43a1b
Daniel Ossorio <daniel@ossorio.me>
2016-01-16 12:03:31 +0000

    using mirage to mock the backend

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
 
-var todos = [
-  {desc: 'Buy bread'},
-  {desc: 'Be a better developer'},
-  {desc: 'Learn emberjs'},
-];
-
 export default Ember.Route.extend({
   model(){
-    return todos;
+    return this.store.findAll('todo');
   },
 });
